47ae1bb29e501710766a269953e8d8b9601eddca
git-richardwu <richardwu720@gmail.com>
2022-06-15 22:08:17 -0400

    Update AboutMe.jsx

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -10,9 +10,9 @@ export default function AboutMe() {
                     <hr></hr>
                         <div className="cardDescription">
                             I am a college graduate holding a Bachelor's Degree in Computer Science from the University at Buffalo. I am a 
-                            first-generation Asian American and I currently reside in the ever-bustling New York City. Growing up, I have always had 
-                            an eye for design given my interest in pursuing an art career. While those ambitions have now become more of a pastime,
-                            I now strive to channel that creativity into developing software and applications.
+                            first-generation Asian American and I currently reside in the ever-bustling New York City. Growing up, an interest in pursuing art as a career led me to develop a
+                            keen eye for detail and design. While those ambitions have now become more of a pastime,
+                            I strive to channel that creativity into developing software and applications.
                             My hobbies include listening to music, cooking, and developing video games.
                         </div>
                 </div>
