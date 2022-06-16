8806dbaf2419206f8a2f963d912add8d949a40f7
git-richardwu <richardwu720@gmail.com>
2022-06-15 21:16:54 -0400

    Updates

---
+++ precache-manifest.54309948f33ef5a235e8a3165191efb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c980561fd86ac6c2938f",
+    "url": "/personal-website/static/css/main.e7286bb0.chunk.css"
+  },
+  {
+    "revision": "c980561fd86ac6c2938f",
+    "url": "/personal-website/static/js/main.c980561f.chunk.js"
+  },
+  {
+    "revision": "01dfb794d16aad399f7b",
+    "url": "/personal-website/static/js/1.01dfb794.chunk.js"
+  },
+  {
+    "revision": "7de3393924c49ef3b14b",
+    "url": "/personal-website/static/js/runtime~main.7de33939.js"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/personal-website/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/personal-website/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "35c3512421f1eb34b7fe7a440245ddd6",
+    "url": "/personal-website/static/media/Resume.35c35124.pdf"
+  },
+  {
+    "revision": "f42e1ddc5d23a57760066c6c6e47b014",
+    "url": "/personal-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.243dfe3094b19ccd7a9aa19ac004b071.js"
+  "/personal-website/precache-manifest.54309948f33ef5a235e8a3165191efb2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/personal-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
