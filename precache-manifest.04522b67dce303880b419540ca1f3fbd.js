482341592e048a686fe51f1714304cb981c63fce
git-richardwu <richardwu720@gmail.com>
2022-06-19 17:06:54 -0400

    Updates

---
+++ precache-manifest.04522b67dce303880b419540ca1f3fbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "035d8c174a6cdf0a07fe",
+    "url": "/personal-website/static/css/main.d1e00dd4.chunk.css"
+  },
+  {
+    "revision": "035d8c174a6cdf0a07fe",
+    "url": "/personal-website/static/js/main.035d8c17.chunk.js"
+  },
+  {
+    "revision": "d46ca7b3241a54b19728",
+    "url": "/personal-website/static/js/1.d46ca7b3.chunk.js"
+  },
+  {
+    "revision": "7de3393924c49ef3b14b",
+    "url": "/personal-website/static/js/runtime~main.7de33939.js"
+  },
+  {
+    "revision": "35c3512421f1eb34b7fe7a440245ddd6",
+    "url": "/personal-website/static/media/Resume.35c35124.pdf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/personal-website/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/personal-website/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "9a9927f1fbce06ef212b045d682470f2",
+    "url": "/personal-website/index.html"
+  }
+];
