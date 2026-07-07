5665c04f7bb7856477daa010b302e3953a0df0af
git-richardwu <richardwu720@gmail.com>
2026-07-07 18:24:46 -0400

    Add load animation to Landing Page

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,6 +1,7 @@
 import './pages/LandingPage.css'
 import { useNavigate } from "react-router-dom";
 import { useTransition } from './context/TransitionContext';
+import { useEffect, useState } from 'react';
 
 interface TileProps {
     color: string,
@@ -11,7 +12,12 @@ interface TileProps {
 
 const Tiles = ({ color, title, to, index }: TileProps) => {
     const { setIsAnimating, setClickX, setClickY, setTransitionColor } = useTransition();
+    const [isLoaded, setIsLoaded] = useState<boolean>(false)
     let navigate = useNavigate();
+
+    useEffect(() => {
+        setIsLoaded(true);
+    })
 
     const handleClick = (e: any) => {
         const { clientX, clientY } = e;
@@ -25,7 +31,7 @@ const Tiles = ({ color, title, to, index }: TileProps) => {
         }, 800);
     };
     return (
-        <div onClick={handleClick} className="tile" style={{ backgroundColor: color, color: '#F7F6F2' }} key={index}>
+        <div onClick={handleClick} className={`tile tile-${index} ${isLoaded ? 'animate' : ''}`} style={{ backgroundColor: color, color: '#F7F6F2' }} key={index}>
             <div className="tile-text" style={{ transform: 'rotate(-45deg)' }} >
                 <h5>{title}</h5>
             </div>
